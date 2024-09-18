7b49300e4033c3b68cf146e92a37212361132593
weiziyu <weizy021@avic.com>
2024-09-18 17:50:09 +0800

    重要接口的入口， 可参照优秀代码 -> BeanFactory

---
+++ src/main/java/cn/analysis/Enter.java
@@ -0,0 +1,17 @@
+package cn.analysis;
+
+
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * @Author ziyu.wei
+ * <p>
+ * 2024/9/18 17:48
+ *
+ * 重要接口的入口， 可参照优秀代码
+ */
+public class Enter {
+
+
+    private BeanFactory beanFactory;
+}
